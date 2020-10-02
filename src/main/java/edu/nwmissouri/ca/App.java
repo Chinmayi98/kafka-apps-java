1545424a107aa756c20519b30116e7d9c4bfb0c6
Chinmayi <ambatichinmayi@gmail.com>
2020-10-02 14:35:34 -0500

    add maven archetype

---
+++ src/main/java/edu/nwmissouri/ca/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.ca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Working on Kafka java apps!" );
+    }
+}
